ae492679803fd6f1287b7b89b8d8d6595d214992
Wallef Antoine <antoine.wallef@gmail.com>
2018-09-05 13:39:04 +0200

    test 1

--- src/Controller/StickersController.php
+++ src/Controller/StickersController.php
@@ -3,26 +3,32 @@ namespace App\Controller;
 
 use App\Controller\AppController;
 use App\Pdf\StickersPDF;
+use League\Csv\Reader;
+use League\Csv\Statement;
 
 class StickersController extends AppController
 {
   public function create()
   {
-    $pdf = new StickersPDF();
-    $pdf->setSticker(65,65, WWW_ROOT.'img'.DS.'front.jpg')
-    ->drawOneStricker()
-    ->drawOneStricker()
-    ->drawOneStricker()
-    ->drawOneStricker()
-    ->drawOneStricker()
-    ->drawOneStricker()
-    ->drawOneStricker()
-    ->drawOneStricker()
-    ->drawOneStricker()
-    ->drawOneStricker()
-    ->drawOneStricker()
-    ->drawOneStricker()
-    ->drawOneStricker()
-    ->save();
+
+    // csv
+    $csv = Reader::createFromPath(TMP.'stickers'.DS.'codes.csv', 'r');
+    $records = $csv->getRecords();
+
+    // only some...
+    //$stmt = (new Statement())->limit(10);
+    //$records = $stmt->process($csv);
+
+    // draw back
+    $back = (new StickersPDF(['pdf.name' => 'back.pdf','font.color' => '#000000']))
+    ->setSticker(65,65, TMP.'stickers'.DS.'back.jpg');
+    foreach ($records as $offset => $record) $back->drawOneStricker($record[0], 27, 47.5);
+    $back->save();
+
+    // draw front
+    $front = (new StickersPDF(['pdf.name' => 'front.pdf']))
+    ->setSticker(65,65, TMP.'stickers'.DS.'front.jpg');
+    for($i = 0; $i <= $offset; $i++) $front->drawOneStricker();
+    $front->save();
   }
 }
--- src/Pdf/StickersPDF.php
+++ src/Pdf/StickersPDF.php
@@ -15,7 +15,7 @@ class StickersPDF
       'unit' => 'mm',
       'size' => 'A4',
       'reverse' => false,
-      'path' => APP.'stickers'.DS,
+      'path' => TMP.'stickers'.DS,
       'name' => 'stickers.pdf',
       'title' => 'Stickers',
       'author' => 'WGR',
@@ -23,6 +23,7 @@ class StickersPDF
 
     'font' => [
       'face' => 'Arial',
+      'style' => 'B', // or ''
       'size' => 8,
       'color' => '#000000'
     ],
@@ -30,7 +31,7 @@ class StickersPDF
     'sticker' => [
       'width' => 74,
       'height' => 105,
-      'background' => WWW_ROOT.'img'.DS.'front.jpg',
+      'background' => TMP.'stickers'.DS.'front.jpg',
     ],
 
     'cropMarks' => [
@@ -52,12 +53,18 @@ class StickersPDF
   {
     $this->setConfig($config);
 
+    // document
     $this->pdf = new PDF($this->getConfig('pdf.orientation'), $this->getConfig('pdf.unit'), $this->getConfig('pdf.size'));
     $this->pdf->SetTitle($this->getConfig('pdf.title'));
     $this->pdf->SetAuthor($this->getConfig('pdf.author'));
     $this->pdf->SetAutoPageBreak(true);
     $this->pdf->AddPage();
 
+    // Text
+    call_user_func_array(array($this->pdf, "SetTextColor"), $this->hexToArray($this->getConfig('font.color')));
+    $this->pdf->SetFont($this->getConfig('font.face'), $this->getConfig('font.style'), $this->getConfig('font.size'));
+
+    // Sticker
     $this->sticker = new Sticker();
     $this->sticker->setWidth($this->getConfig('sticker.width'))
       ->setHeight($this->getConfig('sticker.height'))
@@ -66,14 +73,21 @@ class StickersPDF
 
   public function save()
   {
-    $folder = new Folder($this->getConfig('pdf.path'), true, 0755);
+    $folder = new Folder($this->getConfig('pdf.path'), true, 0777);
     $this->pdf->Output('F',$folder->path.$this->getConfig('pdf.name'));
     return $this;
   }
 
-  public function drawOneStricker()
+  public function drawText($x, $y, $text)
+  {
+    $this->pdf->Text($this->pdf->GetX() + $x , $this->pdf->GetY() + $y, $text);
+    return $this;
+  }
+
+  public function drawOneStricker($text = null, $x = 0, $y = 0)
   {
     $this->_drawStricker();
+    if($text) $this->drawText($x, $y, $text);
     $nextX = $this->pdf->GetX() +  $this->getConfig('margins.left') + $this->sticker->getWidth();
     $nextY = $this->pdf->GetY() +  $this->getConfig('margins.bottom') + $this->sticker->getHeight();
 
@@ -107,20 +121,6 @@ class StickersPDF
     $margin = $this->pdf->GetPageWidth() - ( $number * $width ) - ( ($number - 1) * $this->getConfig('margins.left') );
     $this->pdf->SetTheMargins($margin/2, 10);
 
-    return $this;
-  }
-
-  public function setCode($code)
-  {
-    $this->sticker->setCode($code);
-    return $this;
-  }
-
-  public function setFont($face,$size,$color)
-  {
-    $this->getConfig('font.face', $face);
-    $this->getConfig('font.size', $size);
-    $this->getConfig('font.color', $color);
     return $this;
   }
 
